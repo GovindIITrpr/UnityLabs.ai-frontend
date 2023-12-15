6834dbdf34849fdff0e07ec4ee2eced5620c1d43
govind <shakyagovind230@gmail.com>
2023-12-15 13:47:26 +0530

    Final commit

--- src/Components/PostDetails.jsx
+++ src/Components/PostDetails.jsx
@@ -11,7 +11,7 @@ const PostDetail = () => {
     const fetchPostDetails = async () => {
       try {
         const response = await axios.get(
-          `http://hn.algolia.com/api/v1/items/${objectID}`
+          `//hn.algolia.com/api/v1/items/${objectID}`
         );
 
         setPostDetails(response.data);
